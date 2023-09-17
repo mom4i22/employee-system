119e5c6ae7482c63a8f406831262f595b77bb55b
Momchil <daiparimaina@gmail.com>
2023-09-17 14:58:34 +0300

    update for aws deployment

--- react-employee-system/src/services/EmployeeService.js
+++ react-employee-system/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://18.192.51.52:8080/api/v1/employees";
 
 class EmployeeService {
   saveEmployee(employee) {
